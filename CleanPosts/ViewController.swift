2280d29f76c9e273b199760a5dd6ec30bbd945dd
francisvelueta <fvelueta@outlook.com>
2019-12-07 20:00:02 -0600

    Initial Commit

---
+++ CleanPosts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CleanPosts
+//
+//  Created by Francisca Velueta on 07/12/19.
+//  Copyright © 2019 Francis Velueta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
